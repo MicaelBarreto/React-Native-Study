5a2d5272be36774c808cafa66fdd0031e9e97130
micaelbarreto <micaelplayer98@gmail.com>
2019-04-04 22:44:01 -0300

    Counter and Multi Platform

--- Menu.js
+++ Menu.js
@@ -4,8 +4,16 @@ import { createStackNavigator, createDrawerNavigator, createAppContainer } from 
 import Simple from './src/components/Simple';
 import EvenOdd from './src/components/EvenOdd';
 import { Reverse, Random } from './src/components/Multi';
+import Counter from './src/components/Counter';
+import Platforms from './src/components/Platforms';
 
 const RootStack = createDrawerNavigator({
+    Platforms: {
+        screen: () => <Platforms />
+    },
+    Counter: {
+        screen: () => <Counter />
+    },
     Random: {
         screen: () => <Random numbers={8} />,
         navigationOptions: {title: 'Random'}
